bca97f65041716816f60ad84ce3188f388a8037e
Arielle Villa-massone <avilla-m@e4r5p4.42roma.it>
2023-05-15 16:07:30 +0200

    pointer update

---
+++ submodule.sh
@@ -0,0 +1,17 @@
+git submodule update --init --recursive;
+cd 01_libft         && git checkout master && cd ..;
+cd 02_ftprintf      && git checkout master && cd ..;
+cd 03_get_next_line && git checkout master && cd ..;
+cd 04_netwhat       && git checkout main && cd ..;
+cd 06_push_swap     && git checkout master && cd ..;
+cd 07_fdf           && git checkout master && cd ..;
+cd 08_pipex         && git checkout master && cd ..;
+cd 09_minishell     && git checkout master && cd ..;
+cd 11_philosophers  && git checkout master && cd ..;
+cd 13_cub3D         && git checkout master && cd ..;
+cd 14_piscine_CPP   && git checkout master && cd ..;
+cd 15_netpractice   && git checkout main && cd ..;
+cd 16_ft_containers && git checkout master && cd ..;
+cd 17_inception     && git checkout main && cd ..;
+cd ___exams         && git checkout main && cd ..;
+git add . && git commit -m "pointer update" && git push;
